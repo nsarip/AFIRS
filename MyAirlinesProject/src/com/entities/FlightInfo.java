73df42d2ae62b4307ac0e9b2600d333eef107e33
Nabeel Sarip <nabeel.sarip@gmail.com>
2017-10-13 01:30:41 -0700

    Changes in FlightInfo Class

--- MyAirlinesProject/src/com/entities/FlightInfo.java
+++ MyAirlinesProject/src/com/entities/FlightInfo.java
@@ -1,5 +1,40 @@
 package com.entities;
 
 public class FlightInfo {
+int flightID;
+String source;
+String destination;
+int totalSeats;
+String planeType;
+public int getFlightID() {
+	return flightID;
+}
+public void setFlightID(int flightID) {
+	this.flightID = flightID;
+}
+public String getSource() {
+	return source;
+}
+public void setSource(String source) {
+	this.source = source;
+}
+public String getDestination() {
+	return destination;
+}
+public void setDestination(String destination) {
+	this.destination = destination;
+}
+public int getTotalSeats() {
+	return totalSeats;
+}
+public void setTotalSeats(int totalSeats) {
+	this.totalSeats = totalSeats;
+}
+public String getPlaneType() {
+	return planeType;
+}
+public void setPlaneType(String planeType) {
+	this.planeType = planeType;
+}
 
 }
